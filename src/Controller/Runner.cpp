e78bc51da2e02cfbf21d07a555afa07d4b4eb2bd
Trevor <thod0127@740s-pl-130.local>
2016-01-14 13:54:16 -0700

    Added project to repo

---
+++ src/Controller/Runner.cpp
@@ -0,0 +1,17 @@
+/*
+ * Runner.cpp
+ *
+ *  Created on: Jan 14, 2016
+ *      Author: thod0127
+ */
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	cout << "Hello This is C++" << endl;
+	//Same as System.out.println("Some text");
+
+	return 0;
+}
